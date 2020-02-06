c13e7fc0ef7350b79c5a91d53b05217fa1fd483f
Tali Avni <tali@aristo7.com>
2020-02-05 17:57:46 -0800

    api fetch

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+export const getMonsters = (page) => {
+  return fetch(`https://xfiles-api.herokuapp.com/api/v1/characters?category=Monster_of_the_Week&perPage=20&page=${page}`)
+    .then(res => res.json());
+};
+
+export const getCharacter = (name) => {
+  return fetch(`https://xfiles-api.herokuapp.com/api/v1/characters/${name}`)
+    .then(res => res.json());
+};
